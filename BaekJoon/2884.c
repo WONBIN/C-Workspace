23e1edfde611be8165d09af01ef9797800c9f758
WONBIN <wonbingod20@gmail.com>
2025-09-27 19:24:55 +0900

    Initial Commit

---
+++ BaekJoon/2884.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int h,m;
+    scanf("%d %d",&h,&m);
+    if(m>=45)
+        m = m - 45;
+    else
+    {
+        m = m + 15;
+        if(h == 0)
+            h = 23;
+        else 
+            h = h -1;    
+    }
+   printf("%d %d",h,m);
+   return 0;
+}
+/*Think about optimization of your algorithm.Regardless of you passed on the quiz*/
+ 
